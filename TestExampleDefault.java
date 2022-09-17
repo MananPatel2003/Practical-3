d600b785ed20ab90ac76055fbd6f1e189106f4bb
Manan Patel <96860900+MananPatel2003@users.noreply.github.com>
2022-09-17 23:33:21 +0530

    Add files via upload

---
+++ TestExampleDefault.java
@@ -0,0 +1,6 @@
+public class TestExampleDefault implements ExampleDefault {
+    public static void main(String[] args) {
+        TestExampleDefault a = new TestExampleDefault();
+        a.sample();
+    }
+}
